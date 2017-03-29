3a94948676e404270c588d7b1c9389774e924e3f
Laurențiu Păncescu <laurentiu@laurentiupancescu.com>
2017-03-29 18:07:32 +0200

    Fix PEP8 cosmetic issues

--- endymion/box.py
+++ endymion/box.py
@@ -6,24 +6,24 @@ import urllib2
 
 class Box(object):
     def __init__(self, publisher, name):
-        json_url = "https://atlas.hashicorp.com/{0}/boxes/{1}/"\
-                    .format(publisher, name)
+        json_url = ("https://atlas.hashicorp.com/{0}/boxes/{1}/"
+                    .format(publisher, name))
         request = urllib2.Request(json_url, None,
-                    {'Accept': 'application/json'})
+                                  {'Accept': 'application/json'})
         json_file = urllib2.urlopen(request)
         self._data = json.loads(json_file.read())
 
     def versions(self):
         return tuple(v['version'] for v in self._data['versions']
-                                    if v['status'] == 'active')
+                     if v['status'] == 'active')
 
     def providers(self, version):
         _ver = ([v for v in self._data['versions']
-                    if v['version'] == version])[0]
+                 if v['version'] == version])[0]
         return [p['name'] for p in _ver['providers']]
 
     def url(self, version, provider):
         _ver = ([v for v in self._data['versions']
-                    if v['version'] == version])[0]
+                 if v['version'] == version])[0]
         return ([p for p in _ver['providers']
-                    if p['name'] == provider])[0]['url']
+                 if p['name'] == provider])[0]['url']
--- endymion/policy.py
+++ endymion/policy.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+
 class PolicyError(RuntimeError):
     def __init__(self, message, url):
         self.message = message
